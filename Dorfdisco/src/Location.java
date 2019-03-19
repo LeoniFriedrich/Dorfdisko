b8b0d2a07cdabc8ab0c3ac13458db1842c2e2944
timbe <timbe@Surface-Tim.fritz.box>
2019-03-19 12:00:15 +0100

    asdasd

--- Dorfdisco/src/Location.java
+++ Dorfdisco/src/Location.java
@@ -16,11 +16,11 @@ public class Location {
 	public void setMaxkapazität(int maxkapazität) {
 		this.maxkapazität = maxkapazität;
 	}
-	public String getBlbl() {
-		return blbl;
+	public String getBlbla() {
+		return blbla;
 	}
-	public void setBlbl(String blbl) {
-		this.blbl = blbl;
+	public void setBlbla(String blbla) {
+		this.blbla = blbla;
 	}
 
 
