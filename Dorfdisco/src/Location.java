d0c32e8a547c8a923a1297495dc618102e93967f
timbe <timbe@Surface-Tim.fritz.box>
2019-03-19 11:57:33 +0100

    y<x

--- Dorfdisco/src/Location.java
+++ Dorfdisco/src/Location.java
@@ -4,35 +4,8 @@ public class Location {
 	private String name;
 	private int maxkapazität;
 	private String blbl;
-<<<<<<< HEAD
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getMaxkapazität() {
-		return maxkapazität;
-	}
-	public void setMaxkapazität(int maxkapazität) {
-		this.maxkapazität = maxkapazität;
-	}
-	public String getBlbl() {
-		return blbl;
-	}
-	public void setBlbl(String blbl) {
-		this.blbl = blbl;
-	}
-=======
 	private String blbl4;
 	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
 	public int getMaxkapazität() {
 		return maxkapazität;
 	}
@@ -45,8 +18,14 @@ public class Location {
 	public void setBlbl(String blbl) {
 		this.blbl = blbl;
 	}
+
+
 	
->>>>>>> branch 'master' of https://github.com/TimBeutelspacher/Dorfdisco
-	
-	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
