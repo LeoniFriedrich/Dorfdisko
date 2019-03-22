3410eee6cd0a6f5c60b629ede19bead918652b0c
elina <elina@10.60.46.14>
2019-03-22 12:35:01 +0100

    Code schöner eingerückt.

--- Dorfdisco/src/Gast.java
+++ Dorfdisco/src/Gast.java
@@ -3,18 +3,18 @@ public class Gast extends Person{
   
   private String Hotel;
   private int budget;
-public String getHotel() {
-	return Hotel;
-}
-public void setHotel(String hotel) {
-	Hotel = hotel;
-}
-public int getBudget() {
-	return budget;
-}
-public void setBudget(int budget) {
-	this.budget = budget;
-}
+  public String getHotel() {
+	  return Hotel;
+  }
+  public void setHotel(String hotel) {
+	  Hotel = hotel;
+  }
+  public int getBudget() {
+	  return budget;
+  }
+  public void setBudget(int budget) {
+	  this.budget = budget;
+  }
   
   
   
