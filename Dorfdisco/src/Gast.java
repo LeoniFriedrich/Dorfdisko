e70db1963c69b201637746a871a6ee8d91314b29
fera3 <fera3@LAPTOP-60N6S2P7.fritz.box>
2019-03-22 14:50:37 +0100

    Ok Jonas

--- Dorfdisco/src/Gast.java
+++ Dorfdisco/src/Gast.java
@@ -1,7 +1,6 @@
 
 public class Gast extends Person{
   
-
   private String Hotel;
   private int budget;
   private boolean passend;
--- Dorfdisco/src/Test1.java
+++ Dorfdisco/src/Test1.java
@@ -3,7 +3,7 @@ public class Test1 {
 
 	public static void main(String[]args) {
 		Gast g1 = new Gast("Felix", "Rapp", 19);
-		Location l1 = new Location("Mensa", 15, 100);
+		Location l1 = new Location("Abfahrtsclub", 15, 100);
 		
 		g1.inClubGehen(l1);
 		
