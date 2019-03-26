33dc6458a440b39cdf4f5577076ca980c1823529
fera3 <fera3@LAPTOP-60N6S2P7.fritz.box>
2019-03-26 12:49:06 +0100

    Tim auch da

--- Dorfdisco/src/Test1.java
+++ Dorfdisco/src/Test1.java
@@ -3,9 +3,11 @@ public class Test1 {
 
 	public static void main(String[]args) {
 		Gast g1 = new Gast("Felix", "Rapp", 19);
+		Gast g2 = new Gast("Tim", "Currywurst", 77);
 		Location l1 = new Location("Abfahrtsclub", 15, 100);
 		
 		g1.inClubGehen(l1);
+		g2.inClubGehen(l1);
 		
 	}
 	
