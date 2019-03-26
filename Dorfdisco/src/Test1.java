1d34c98dbcefbcdbc8869a1530707eb73955c381
Leo Friedrich <Leo Friedrich@10.60.13.185>
2019-03-26 13:27:55 +0100

    kk

--- Dorfdisco/src/Test1.java
+++ Dorfdisco/src/Test1.java
@@ -5,7 +5,7 @@ public class Test1 {
 		Gast g1 = new Gast("Felix", "Rapp", 19);
 		Gast g2 = new Gast("Tim", "Currywurst", 77);
 		Location l1 = new Location("Abfahrtsclub", 15, 100);
-		
+		Location l2 = new Location("Abfahrtsclub", 15, 100);
 		g1.inClubGehen(l1);
 		g2.inClubGehen(l1);
 		
